562110199b010e7eaf9783da9abf32c99a44ea47
bonbo0811 <abcdef999gp811@gmail.com>
2022-09-24 21:54:06 +0900

    index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sample Site ①</title>
+</head>
+
+<body>
+    <div class="header">
+        <h1>ヘッダー</h1>
+    </div>
+    <div class="main">
+        <p>メイン</p>
+    </div>
+    <div class="footer">
+        <p>フッダー</p>
+    </div>
+</body>
+
+</html>
