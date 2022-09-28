99a88d12ac58cb17e81c8bf8935d7aceda8dadbf
bonbo0811 <abcdef999gp811@gmail.com>
2022-09-28 13:17:34 +0900

    ヘッダー完成 / main pic追加

--- index.php
+++ index.php
@@ -13,32 +13,33 @@
         <h1>sample</h1>
         <div class="nav">
             <ul>
-                <li>About</li>
-                <li>Hobby</li>
+                <li><a href="#About">About</a></li>
+                <li><a href="#Hobby">Hobby</a></li>
             </ul>
         </div>
     </div>
     <div class="main">
         <div class="mainvisual">
-
-        </div class="sec">
-        <section class="About">
-            <h2>About</h2>
-            <p>テキストテキストテキスト</p>
-            <div class="profile">
-                <h3>
-
-                </h3>
-            </div>
-        </section>
-        <section class="Hobby">
-            <h2>Hobby</h2>
-            <ul class="hobbyList">
-                <li>タイトル①</li>
-                <li>タイトル②</li>
-                <li>タイトル③</li>
-            </ul>
-        </section>
+            <img src="./images/camp_pic.jpg" alt="キャンプの画像">
+        </div>
+        <div class="sec">
+            <section class="About">
+                <div class="profile">
+                    <h2><a id="About"><span class="under">About</span></a></h2>
+                    <p>テキストテキストテキスト</p>
+                </div>
+            </section>
+            <section class="Hobby">
+                <div class="Hobby">
+                    <h2><a id="Hobby"><span class="under">Hobby</span></a></h2>
+                    <ul class="hobbyList">
+                        <li>タイトル①</li>
+                        <li>タイトル②</li>
+                        <li>タイトル③</li>
+                    </ul>
+                </div>
+            </section>
+        </div>
     </div>
     <div class="footer">
         <p>2022 kent_okubo</p>
