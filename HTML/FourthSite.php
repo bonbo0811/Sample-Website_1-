0e3ba502d89054f2b99d487f5e18d251dd34f072
bonbo0811 <abcdefg999gp811@gmail.com>
2023-07-14 12:01:49 +0900

    ヘッダー作成

--- HTML/FourthSite.php
+++ HTML/FourthSite.php
@@ -11,13 +11,34 @@
 </head>
 <body>
     <div class="header">
-        <h1>header</h1>
+        <nav class="navbar navbar-light fixed-top">
+            <!-- タイトル -->
+            <h1><a class="navbar-brand" href="#">Furniture Design</a></h1>
+            <!-- ハンバーガーメニュー -->
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <!-- ナビゲーションメニュー -->
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+                </li>
+            </ul>
+        </nav>
     </div>
+
     <div class="main">
         <h2>main</h2>
     </div>
+
     <div class="footer">
         <h2>footer</h2>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
 </body>
 </html> 
